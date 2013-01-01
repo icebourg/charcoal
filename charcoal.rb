53ab28cf8a9f3ceb0e2db4882cd2c0e4607b64b7
AJ Bourg <aj.bourg@sendgrid.com>
2012-12-31 17:41:58 -0700

    Add index page

--- charcoal.rb
+++ charcoal.rb
@@ -4,6 +4,12 @@ require 'haml'
 require 'yaml'
 require File.join(File.expand_path(File.dirname(__FILE__)), "slug.rb")
 
+get '/' do
+  @data = YAML.load_file "config.yml"
+  puts @data
+  haml :index, :locals => {:data => @data}
+end
+
 get '/all' do
   @data = YAML.load_file "config.yml"
   puts @data
