f134643c8dff3f7cb55afd94d206b9e6b1e5a630
AJ Bourg <aj.bourg@sendgrid.com>
2012-12-31 18:22:47 -0700

    Updates style. Adds individual and index routes.

--- charcoal.rb
+++ charcoal.rb
@@ -6,12 +6,23 @@ require File.join(File.expand_path(File.dirname(__FILE__)), "slug.rb")
 
 get '/' do
   @data = YAML.load_file "config.yml"
-  puts @data
   haml :index, :locals => {:data => @data}
 end
 
 get '/all' do
   @data = YAML.load_file "config.yml"
-  puts @data
   haml :stats, :locals => {:data => @data}
+end
+
+get '/:server' do
+  @config = YAML.load_file "config.yml"
+  @data = {
+    'Title' => @config['Title'],
+    'RenderURL' => @config['RenderURL'],
+    params[:server] => @config[params[:server]]
+    }
+  puts @data.to_yaml
+  puts "Original config::::"
+  puts @config.to_yaml
+  haml :stats, :locals => {:data => @data }
 end
